4a2775d1620cedd358c4552ebf18153816b49f74
Aidan <aidanjsnyder@gmail.com>
2026-03-03 19:20:58 -0500

    Edge case for day 62 added

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const PORT = 8080;
 const dayOfYear = Math.floor((Date.now() - new Date(new Date().getFullYear(),0,0)) / 86400000);
 
 app.get('/day', (req, res)=>{
-    res.status(200).send(dayOfYear)
+    if (dayOfYear == 62) {
+        res.status(200).send({
+        word: 'mathematics',
+        definition: 'the abstract science of number, quantity, and space, either as abstract concepts (pure mathematics), or as applied to other disciplines such as physics and engineering (applied mathematics)',
+        part_of_speech: 'Noun',
+        example: 'a taste for mathematics'
+        })
+    }
 })
 
 app.listen(PORT, () => {
