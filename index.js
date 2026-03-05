049234d83bc4345ea1d22bd65428fc9601748fff
Aidan <aidanjsnyder@gmail.com>
2026-03-05 11:23:35 -0500

    Enables cross-origin requests

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+app.use(cors()); // <-- enables cross-origin requests
 
 // 366 entries (index 0 => day 1)
 const WORDS = [
